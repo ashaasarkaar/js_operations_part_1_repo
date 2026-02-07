e9739e3d422c81f49e5c406ee3dbaa356730723a
ashaasarkaar <ashaasarkaar@gmail.com>
2026-02-07 15:02:33 +0600

    complex accurate leapyear calculation

--- complex_leapyear.js
+++ complex_leapyear.js
@@ -1,13 +1,15 @@
 // if the year is divisible by 400, then it is a leap year, else it is not a leap year
 
-function isLeapYear(year){
-    if(year % 4 === 0){
+function isLeapYear(year) {
+    if (year % 100 !== 0 && year % 4 === 0) {
         return true;
     }
-    else{
+    if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }
+    else {
         return false;
     }
 }
-
 const isLeapYearTrue = isLeapYear(2033);
 console.log(isLeapYearTrue);
