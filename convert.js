be91264336422756e3805751f9d23ed7def66e23
ashaasarkaar <ashaasarkaar@gmail.com>
2026-02-07 13:22:45 +0600

    inche to feet with fraction with string

--- convert.js
+++ convert.js
@@ -1,7 +1,11 @@
 function incheToFeet(inche){
     const feet = inche / 12;
-    return feet;
+    const feetFraction = parseInt(feet);
+    const feetRemainder = inche % 12;
+    const finalResult = (feetFraction + ' feet ' + feetRemainder + ' inche ');
+    return finalResult;
 }
 
 const salaatHeight = incheToFeet(75);
 console.log(salaatHeight);
+
