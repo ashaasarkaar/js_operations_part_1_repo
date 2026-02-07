fca6f65b96b39279fa6d387da16422444d4b984a
ashaasarkaar <ashaasarkaar@gmail.com>
2026-02-07 20:43:47 +0600

    Explore Time Zone In js

---
+++ time_Zone.js
@@ -0,0 +1,13 @@
+const date = new Date();
+console.log(date)
+
+const dateString = new Date('2004-08-05')
+console.log(dateString)
+console.log(dateString.getDay())
+console.log(dateString.getMonth())
+console.log(dateString.getFullYear())
+console.log(dateString.toLocaleString())
+console.log(dateString.toLocaleDateString())
+console.log(dateString.toLocaleString('en-GB'))
+console.log(dateString.toLocaleString('bn-BD'))
+
